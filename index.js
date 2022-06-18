65c4746ccd0edfeb4d0dc1c876ca5302e8ea9869
Adam Podoxin <adam.podoxin@gmail.com>
2022-06-17 20:23:40 -0700

    Adjust page styling

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
-$(document).ready(function(){
-  $("a").on('click', function(event) {
-    if (this.hash !== "") {
-      event.preventDefault();
-      var hash = this.hash;
-      $('body,html').animate({
-      scrollTop: $(hash).offset().top
-      }, 1200, function(){
-      window.location.hash = hash;
-     });
-     } 
-    });
+$(document).ready(function () {
+	$("a").on("click", function (event) {
+		let hash = this.hash;
+
+		if (hash !== "") {
+			event.preventDefault();
+
+			$("body,html").animate({ scrollTop: $(hash).offset().top }, 1200, () => {
+				window.location.hash = hash;
+			});
+		}
+	});
 });
 
-var width = $(window).width(); 
+let width = $(window).width();
 
-window.onscroll = function(){
-if ((width >= 900)){
-    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        $("#middle").css("background-size","150% auto");
-    }else{
-        $("#middle").css("background-size","100% auto");        
-    }
-}
+window.onscroll = () => {
+	if (width >= 900) {
+		if (
+			document.body.scrollTop > 80 ||
+			document.documentElement.scrollTop > 80
+		) {
+			$("#middle").css("background-size", "150% auto");
+		} else {
+			$("#middle").css("background-size", "100% auto");
+		}
+	}
 };
 
-setTimeout(function(){
-    $("#loading").addClass("animated fadeOut");
-    setTimeout(function(){
-      $("#loading").removeClass("animated fadeOut");
-      $("#loading").css("display","none");
-    },800);
-},1450);
+setTimeout(() => {
+	$("#loading").addClass("animated fadeOut");
+
+	setTimeout(() => {
+		$("#loading").removeClass("animated fadeOut");
+		$("#loading").css("display", "none");
+	}, 800);
+}, 1450);
