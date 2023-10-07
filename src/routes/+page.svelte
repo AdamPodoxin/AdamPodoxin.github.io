1976d982f9321172926f8803ba829efcd2a3d0f8
Adam Podoxin <adam.podoxin@gmail.com>
2023-10-07 15:09:27 -0700

    Create styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
-	import Repo from '$lib/components/Repo.svelte';
-	import '$lib/styles/global.css';
-
 	import type { PageData } from './$types';
+
+	import Repo from '$lib/components/Repo.svelte';
+	import GitHub from '$lib/icons/GitHub.svelte';
+	import Mail from '$lib/icons/Mail.svelte';
+
+	import '$lib/styles/global.css';
 
 	export let data: PageData;
 	const { repos } = data;
@@ -10,8 +13,38 @@
 	console.log(repos);
 </script>
 
-<div class="repo">
+<header>
+	<section class="title">
+		<h1>Adam Podoxin</h1>
+		<h4>Versatile programmer and perpetual learner</h4>
+	</section>
+
+	<section class="icons">
+		<a href="mailto:adam.podoxin@gmail.com" target="_blank" rel="noreferrer"><Mail /></a>
+		<a href="https://github.com/AdamPodoxin" target="_blank" rel="noreferrer"><GitHub /></a>
+	</section>
+</header>
+
+<section class="repos">
 	{#each repos as repo}
 		<Repo {repo} />
 	{/each}
-</div>
+</section>
+
+<style>
+	.title {
+		margin: 32px;
+	}
+
+	h1,
+	h4 {
+		text-align: center;
+		margin: 8px;
+	}
+
+	.icons {
+		display: flex;
+		justify-content: center;
+		gap: 32px;
+	}
+</style>
