0b05c27119922a12afb92e54b8576b1b530002d6
Adam Podoxin <adam.podoxin@gmail.com>
2024-10-25 22:49:58 -0700

    Update topic formatting

--- src/lib/util/topicsUtil.ts
+++ src/lib/util/topicsUtil.ts
@@ -1,33 +1,27 @@
-export const processTopic = (str: string) => {
-	switch (str) {
-		case 'csharp':
-			return 'C#';
-		case 'html':
-			return 'HTML';
-		case 'css':
-			return 'CSS';
-		case 'javascript':
-			return 'JavaScript';
-		case 'typescript':
-			return 'TypeScript';
-		case 'nextjs':
-			return 'Next.js';
-		case 'nodejs':
-			return 'Node.js';
-		case 'discordjs':
-			return 'Discord.js';
-		case 'fly':
-			return 'Fly.io';
-		case 'fastapi':
-			return 'FastAPI';
-		case 'sveltekit':
-			return 'SvelteKit';
-		default:
-			return str
-				.split('-')
-				.map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-				.join(' ');
+const topicsFormattedMap = new Map<string, string>();
+topicsFormattedMap.set('csharp', 'C#');
+topicsFormattedMap.set('html', 'HTML');
+topicsFormattedMap.set('css', 'CSS');
+topicsFormattedMap.set('javascript', 'JavaScript');
+topicsFormattedMap.set('typescript', 'TypeScript');
+topicsFormattedMap.set('nextjs', 'Next.js');
+topicsFormattedMap.set('nodejs', 'Node.js');
+topicsFormattedMap.set('discordjs', 'Discord.js');
+topicsFormattedMap.set('fly', 'Fly.io');
+topicsFormattedMap.set('fastapi', 'FastAPI');
+topicsFormattedMap.set('sveltekit', 'SvelteKit');
+
+export const formatTopic = (topic: string) => {
+	const topicFormatted = topicsFormattedMap.get(topic);
+
+	if (topicFormatted) {
+		return topicFormatted;
 	}
+
+	return topic
+		.split('-')
+		.map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+		.join(' ');
 };
 
 const topicsOrder = [
@@ -54,6 +48,7 @@ const topicsOrder = [
 	'fastapi',
 	'uvicorn'
 ] as const;
+
 const topicsOrderMap = new Map<string, number>();
 topicsOrder.forEach((topic, i) => topicsOrderMap.set(topic, i + 1));
 
